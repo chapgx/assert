fc995b23ea1f5f74b57665544668580ef6f6804f
Richard <richard@codeh.io>
2025-05-04 13:42:56 -0400

    Fixed issue with message parameter allowing any type
    For Assert will try decode error, string, or internal msg interface (String() string)
    
    For AssertT will passed any value and let *testing.T.Error(v any) handle it

--- assert.go
+++ assert.go
@@ -3,18 +3,46 @@ Simple assertion package
 */
 package assert
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+type msg interface {
+	String() string
+}
 
 // General assert on condition
-func Assert(condition bool, message string) {
-	if !condition {
-		panic("assert failed: " + message)
+func Assert(condition bool, message any) {
+	if condition {
+		return
 	}
+
+	s, ok := message.(string)
+	if ok {
+		panic("assert failed: " + s)
+	}
+
+	e, ok := message.(error)
+	if ok {
+		panic("assert failed: " + e.Error())
+	}
+
+	m, ok := message.(msg)
+	if ok {
+		panic("assert failed: " + m.String())
+	}
+
+	fmt.Println("assert failed unable to decode message paramter panic with raw value")
+	panic(message)
+
 }
 
 // Assert fot testing
-func AssertT(t *testing.T, condition bool, message string) {
-	if !condition {
-		t.Error(message)
+func AssertT(t *testing.T, condition bool, message any) {
+	if condition {
+		return
 	}
+
+	t.Error(message)
 }
